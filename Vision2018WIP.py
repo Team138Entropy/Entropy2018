6c11c5b3e30c21cc37c47941e2bd3378ad527374
Sanctus-Lupus <35547370+Sanctus-Lupus@users.noreply.github.com>
2018-01-30 19:50:30 -0500

    Updated with Hough line Transform (partially functional) and peak based target location (reflections causing severe errors)

--- Vision2018WIP.py
+++ Vision2018WIP.py
@@ -41,8 +41,8 @@ def processImage(img):
 
 #Colors++++++++++++++++++++++++++++++++++++++++++
 #test red or blue
-    redStart = True
-
+    redStart = False
+    findScale = False
 #blue
     if not (redStart):
         lower_blue = np.array([99,100,100])
@@ -59,7 +59,7 @@ def processImage(img):
         mask1 = cv2.inRange(img1, lower_red, upper_red)
     
         mask = cv2.bitwise_or(mask,mask1)
-    kernel = np.ones((1,3), np.uint8)
+    kernel = np.ones((7,7), np.uint8)
     # mask off the grayscale image
     gray = img1[:,:,2]
     ret = cv2.bitwise_and(gray,gray, mask= mask)
@@ -67,7 +67,16 @@ def processImage(img):
 #    ret = cv2.bitwise_and(ret,gray)
     #threshhold test
     thresholdImage, ret = cv2.threshold(ret,127,255,cv2.THRESH_BINARY)
+    if (findScale):
+        edges = cv2.Canny(ret,50,150,apertureSize = 3)
+        minLineLength = 100
+        maxLineGap = 10
+        lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+        for x1,y1,x2,y2 in lines[0]:
+            cv2.line(img,(x1,y1),(x2,y2),(0,255,255),2)
 
+
+#        cv2.imwrite('hough;ines3.jpg',img)
     cv2.imshow('ret',ret)
     
 
@@ -78,20 +87,27 @@ def processImage(img):
     xsum = np.sum(ret,0)
     ysum = np.sum(ret,1)
     
-    findPeaks = False
+    lookforPeaks = True
     targetFound = 0
-    if (findPeaks):
+    if (lookforPeaks):
         xpeaks = findPeaks(xsum)
         ypeaks = findPeaks(ysum)
         print 'X=',xpeaks,'    Y=',ypeaks
         
         
-        if len(xpeaks) == 2 and len(ypeaks) > 0:
+
+        if len(xpeaks) == 1  and len(ypeaks) > 0:
+            xend = len(xpeaks)-1
+            xcenter = xpeaks[0][0] + (xpeaks[xend][1]-xpeaks[0][0]) / 2
+            yend = len(ypeaks)-1
+            ycenter = ypeaks[0][0] + (ypeaks[yend][1]-ypeaks[0][0]) / 2
+            targetFound = 1
+        elif len(xpeaks)==2 and len(ypeaks) >0:
+            xend = len(xpeaks)-1
             xcenter = xpeaks[0][1] + (xpeaks[1][0]-xpeaks[0][1]) / 2
             yend = len(ypeaks)-1
             ycenter = ypeaks[0][0] + (ypeaks[yend][1]-ypeaks[0][0]) / 2
             targetFound = 1
-            
     
     #testing things
     #grayContours = locateContours(ret,0,0)
@@ -103,7 +119,7 @@ def processImage(img):
         drawCrosshair(img,xcenter,ycenter,True)
         
         
-    if (findPeaks):
+    if (lookforPeaks):
         plt.figure(1)
         plt.subplot(2,1,1)
         plt.plot(xsum)
@@ -114,7 +130,7 @@ def processImage(img):
         
     cv2.imshow('Input',img)
     
-    if (findPeaks):
+    if (lookforPeaks):
         plt.show(10)
     
     return ret
