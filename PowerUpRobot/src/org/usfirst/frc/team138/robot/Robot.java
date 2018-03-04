068a08dabe0b02bfdbd173cbdf6a4ab56a8d57b1
Liam Daley <liamdaley276@gmail.com>
2018-03-04 12:41:01 -0500

    Disabled the climber inside robot.java - re-enabling it only requires the un-commenting of two lines in that file.

--- PowerUpRobot/src/org/usfirst/frc/team138/robot/Robot.java
+++ PowerUpRobot/src/org/usfirst/frc/team138/robot/Robot.java
@@ -33,7 +33,7 @@ public class Robot extends IterativeRobot {
     public static final Drivetrain drivetrain = new Drivetrain();
     public static final Grasper grasper = new Grasper();
     public static final Elevator elevator = new Elevator();
-    public static final Climber climber = new Climber();
+    // public static final Climber climber = new Climber();
 
     public static final OI oi = new OI();
 	
@@ -171,7 +171,7 @@ public class Robot extends IterativeRobot {
 		
         Sensors.updateSmartDashboard();
         elevator.updateSmartDashboard();
-        climber.updateSmartDashboard();
+        // climber.updateSmartDashboard();
     }
     
     /**
