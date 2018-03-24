0202d76eca2d7c7c788339fd44850d6ce23f418a
Jim McGinness <jmcgnh@gmail.com>
2018-03-24 17:03:42 -0400

    Post camera parameters on SmartDashboard.

--- PowerUpRobot/src/org/usfirst/frc/team138/robot/Sensors.java
+++ PowerUpRobot/src/org/usfirst/frc/team138/robot/Sensors.java
@@ -39,7 +39,11 @@ public class Sensors {
 	// JeVois camera selects which script to run based on VideoMode
 	// This VideoMode selects the TestPython.py script which, in the default load, slightly decorates the
 	// image with a circle and a string "Hi from Python", but otherwise passes through the camera image unchanged.
-	Camera0.setVideoMode(PixelFormat.kMJPEG, 320, 240, 15);
+	   Camera0.setVideoMode(PixelFormat.kMJPEG, 320, 240, 15);
+	   SmartDashboard.putString("PixelFormat", "MJPEG");
+	   SmartDashboard.putNumber("x-resolution", 320);
+	   SmartDashboard.putNumber("y-resolution",240);     
+	   SmartDashboard.putNumber("framerate",30);     
 
 	}
 	
